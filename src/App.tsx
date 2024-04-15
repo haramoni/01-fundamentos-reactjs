372d421a7e8691b9de626f51bb1e5a3d402c0d0b
Júlia Haramoni <haramoni.julia@gmail.com>
2024-04-15 10:27:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <Post />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,9 @@
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" alt="" />
+    </div>
+  );
+}
